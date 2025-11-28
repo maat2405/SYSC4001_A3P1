497cad0641630e85db8bc9cdbc0a34316c41e9e5
Maathusan Sathiendran <155667928+maat2405@users.noreply.github.com>
2025-11-28 16:30:59 -0500

    Changed filenames to include student numbers

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ if [ ! -d "bin" ]; then
 else
 	rm bin/*
 fi
-
-g++ -g -O0 -I . -o bin/interrupts_EP interrupts_student1_student2_EP.cpp
-g++ -g -O0 -I . -o bin/interrupts_RR interrupts_student1_student2_RR.cpp
-g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_student1_student2_EP_RR.cpp
+h
+g++ -g -O0 -I . -o bin/interrupts_EP interrupts_101306866_101302780_EP.cpp
+g++ -g -O0 -I . -o bin/interrupts_RR interrupts_101306866_101302780_RR.cpp
+g++ -g -O0 -I . -o bin/interrupts_EP_RR interrupts_101306866_101302780_EP_RR.cpp
--- interrupts_101306866_101302780_EP_RR.cpp
+++ interrupts_101306866_101302780_EP_RR.cpp
@@ -5,7 +5,7 @@
  * 
  */
 
-#include "interrupts_student1_student2.hpp"
+#include "interrupts_101306866_101302780.hpp"
 
 void FCFS(std::vector<PCB> &ready_queue) {
     std::sort( 
